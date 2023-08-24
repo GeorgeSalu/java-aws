caf382546941d0fd2b5235c1039e9e5b6c8079ea
GeorgeSalu <george.salu10@gmail.com>
2023-08-24 20:50:33 -0300

    projeto park-api - incluindo equals, hashcode e toString na entidade de usuario

--- park-api/src/main/java/com/example/parkapi/entity/Usuario.java
+++ park-api/src/main/java/com/example/parkapi/entity/Usuario.java
@@ -2,6 +2,7 @@ package com.example.parkapi.entity;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 import jakarta.persistence.*;
 
@@ -13,35 +14,35 @@ public class Usuario implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id")
 	private Long id;
-	
+
 	@Column(name = "username", nullable = false, unique = true, length = 100)
 	private String username;
-	
+
 	@Column(name = "password", nullable = false, length = 200)
 	private String password;
-	
+
 	@Enumerated(EnumType.STRING)
 	@Column(name = "role", nullable = false, length = 25)
 	private Role role;
-	
+
 	@Column(name = "data_criacao")
 	private LocalDateTime dataCriacao;
-	
+
 	@Column(name = "data_modificacao")
 	private LocalDateTime dataModificacao;
-	
+
 	@Column(name = "criado_por")
 	private String criadoPor;
-	
+
 	@Column(name = "modificado_por")
 	private String modificadoPor;
-	
+
 	public enum Role {
 		ROLE_ADMIN, ROLE_CLIENTE
 	}
-	
-	public Usuario() { }
-	
+
+	public Usuario() {
+	}
 
 	public Long getId() {
 		return id;
@@ -107,6 +108,28 @@ public class Usuario implements Serializable {
 		this.modificadoPor = modificadoPor;
 	}
 
-	
-	
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "Usuario [id=" + id + ", username=" + username + ", password=" + password + ", role=" + role
+				+ ", dataCriacao=" + dataCriacao + ", dataModificacao=" + dataModificacao + ", criadoPor=" + criadoPor
+				+ ", modificadoPor=" + modificadoPor + "]";
+	}
+
 }
