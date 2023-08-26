6cd2b31743966cac14abad26494735ce0eb6c9e6
GeorgeSalu <george.salu10@gmail.com>
2023-08-26 09:46:30 -0300

    projeto park-api - removendo veriavel não utilizada

--- park-api/src/main/java/com/example/parkapi/web/controller/UsuarioController.java
+++ park-api/src/main/java/com/example/parkapi/web/controller/UsuarioController.java
@@ -43,7 +43,7 @@ public class UsuarioController {
 	
 	@PatchMapping("/{id}")
 	public ResponseEntity<Void> updatePassword(@PathVariable Long id,@RequestBody UsuarioSenhaDto dto) {
-		Usuario user = usuarioService.editarSenha(id, dto.getSenhaAtual(), dto.getNovaSenha(), dto.getConfirmaSenha());
+		usuarioService.editarSenha(id, dto.getSenhaAtual(), dto.getNovaSenha(), dto.getConfirmaSenha());
 		return ResponseEntity.noContent().build();
 	}
 	
