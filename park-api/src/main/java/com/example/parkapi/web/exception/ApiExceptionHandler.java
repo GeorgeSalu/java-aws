c84fb554d26fdbc97b092c20086406610d8bd9de
GeorgeSalu <george.salu10@gmail.com>
2023-08-29 18:09:02 -0300

    projeto park-api - logando erro para fica mas claro onde estorou o error

--- park-api/src/main/java/com/example/parkapi/web/exception/ApiExceptionHandler.java
+++ park-api/src/main/java/com/example/parkapi/web/exception/ApiExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.example.parkapi.web.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -12,11 +14,14 @@ import jakarta.servlet.http.HttpServletRequest;
 
 @RestControllerAdvice
 public class ApiExceptionHandler {
+	
+	private static final Logger log = LoggerFactory.getLogger(ApiExceptionHandler.class);
 
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 	public ResponseEntity<ErrorMessage> methodArgumentNotValidException(MethodArgumentNotValidException ex, 
 																		HttpServletRequest request,
 																		BindingResult result) {
+		log.error("Api Error - "+ex);
 		return ResponseEntity
 					.status(HttpStatus.UNPROCESSABLE_ENTITY)
 					.contentType(MediaType.APPLICATION_JSON)
