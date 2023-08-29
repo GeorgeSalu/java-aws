f3d12748206bfff3ad6cb947942600fb4ff26ab6
GeorgeSalu <george.salu10@gmail.com>
2023-08-29 18:31:58 -0300

    projeto park-api - incluindo serialVersionId nas exceptions customizadas

--- park-api/src/main/java/com/example/parkapi/exception/EntityNotFoundException.java
+++ park-api/src/main/java/com/example/parkapi/exception/EntityNotFoundException.java
@@ -2,6 +2,8 @@ package com.example.parkapi.exception;
 
 public class EntityNotFoundException extends RuntimeException{
 
+	private static final long serialVersionUID = -4067758293625920219L;
+
 	public EntityNotFoundException(String message) {
 		super(message);
 	}
--- park-api/src/main/java/com/example/parkapi/exception/UsernameUniqueViolationException.java
+++ park-api/src/main/java/com/example/parkapi/exception/UsernameUniqueViolationException.java
@@ -2,6 +2,8 @@ package com.example.parkapi.exception;
 
 public class UsernameUniqueViolationException extends RuntimeException {
 
+	private static final long serialVersionUID = 569323222816955168L;
+
 	public UsernameUniqueViolationException(String message) {
 		super(message);
 	}
