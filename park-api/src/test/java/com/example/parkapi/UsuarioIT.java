4b7a923f31e0b1e108d890f9c4fb0acda6133927
GeorgeSalu <george.salu10@gmail.com>
2023-08-31 18:14:36 -0300

    projeto park-api - configurando o ambiente de teste

---
+++ park-api/src/test/java/com/example/parkapi/UsuarioIT.java
@@ -0,0 +1,18 @@
+package com.example.parkapi;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@Sql(scripts = "/sql/usuarios/usuario-insert.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(scripts = "/sql/usuarios/usuario-delete.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class UsuarioIT {
+
+	@Autowired
+	WebTestClient testClient;
+	
+	
+	
+}
