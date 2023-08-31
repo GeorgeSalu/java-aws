67d4639f6e5a5f9614d85c40b37ab4d1aab7de7d
GeorgeSalu <george.salu10@gmail.com>
2023-08-31 18:27:31 -0300

    projeto park-api - corrige pacote resource de teste

--- park-api/src/test/java/com/example/parkapi/UsuarioIT.java
+++ park-api/src/test/java/com/example/parkapi/UsuarioIT.java
@@ -1,18 +1,39 @@
 package com.example.parkapi;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
-@Sql(scripts = "/sql/usuarios/usuario-insert.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
-@Sql(scripts = "/sql/usuarios/usuario-delete.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+import com.example.parkapi.web.dto.UsuarioCreateDto;
+import com.example.parkapi.web.dto.UsuarioResponseDto;
+
+@Sql(scripts = "/sql/usuarios/usuarios-insert.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(scripts = "/sql/usuarios/usuarios-delete.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class UsuarioIT {
 
 	@Autowired
 	WebTestClient testClient;
 	
-	
+	@Test
+	public void createUsuario_ComUsernameEPasswordValidos_RetornarUsuarioCriadoComStatus201() {
+		UsuarioResponseDto responseBody = testClient
+			.post()
+			.uri("/api/v1/usuarios")
+			.contentType(MediaType.APPLICATION_JSON)
+			.bodyValue(new UsuarioCreateDto("tody@gmail.com","123456"))
+			.exchange()
+			.expectStatus().isCreated()
+			.expectBody(UsuarioResponseDto.class)
+			.returnResult().getResponseBody();
+		
+		Assertions.assertThat(responseBody).isNotNull();
+		Assertions.assertThat(responseBody.getId()).isNotNull();
+		Assertions.assertThat(responseBody.getUsername()).isNotNull();
+	}
 	
 }
