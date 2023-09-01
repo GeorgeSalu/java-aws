df5199a004d3fe8c80054018759fdc56f81f1ffc
GeorgeSalu <george.salu10@gmail.com>
2023-09-01 17:50:47 -0300

    projeto park-api - a classe JwtUserDetailsService

--- park-api/src/main/java/com/example/parkapi/repository/UsuarioRepository.java
+++ park-api/src/main/java/com/example/parkapi/repository/UsuarioRepository.java
@@ -1,9 +1,17 @@
 package com.example.parkapi.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.example.parkapi.entity.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+	Optional<Usuario> findByUsername(String username);
+
+	@Query(" select u.role from Usuario u where u.username like :username ")
+	Usuario.Role findByRoleByUsername(String username);
 
 }
--- park-api/src/main/java/com/example/parkapi/service/UsuarioService.java
+++ park-api/src/main/java/com/example/parkapi/service/UsuarioService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.parkapi.entity.Usuario;
+import com.example.parkapi.entity.Usuario.Role;
 import com.example.parkapi.exception.EntityNotFoundException;
 import com.example.parkapi.exception.PasswordInvalidException;
 import com.example.parkapi.exception.UsernameUniqueViolationException;
@@ -54,6 +55,18 @@ public class UsuarioService {
 	@Transactional(readOnly = true)
 	public List<Usuario> buscarTodos() {
 		return usuarioRepository.findAll();
+	}
+
+	@Transactional(readOnly = true)
+	public Usuario buscarPorUsername(String username) {
+		return usuarioRepository.findByUsername(username).orElseThrow(
+				() -> new EntityNotFoundException(String.format("Usuario com '%s' não encontrado", username))
+		);
+	}
+
+	@Transactional(readOnly = true)
+	public Usuario.Role buscarRolePorUsername(String username) {
+		return usuarioRepository.findByRoleByUsername(username);
 	}
 	
 	
