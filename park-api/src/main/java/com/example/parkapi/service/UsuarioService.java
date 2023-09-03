3d5f32b336012334e76f5169be543e69bb12a80a
GeorgeSalu <george.salu10@gmail.com>
2023-09-03 10:29:08 -0300

    projeto park-api - ajustes finais para o processo de autenticacao

--- park-api/src/main/java/com/example/parkapi/service/UsuarioService.java
+++ park-api/src/main/java/com/example/parkapi/service/UsuarioService.java
@@ -3,6 +3,7 @@ package com.example.parkapi.service;
 import java.util.List;
 
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,14 +17,17 @@ import com.example.parkapi.repository.UsuarioRepository;
 public class UsuarioService {
 
 	private final UsuarioRepository usuarioRepository;
+	private final PasswordEncoder passwordEncoder;
 
-	public UsuarioService(UsuarioRepository usuarioRepository) {
+	public UsuarioService(UsuarioRepository usuarioRepository, PasswordEncoder passwordEncoder) {
 		this.usuarioRepository = usuarioRepository;
+		this.passwordEncoder = passwordEncoder;
 	}
 
 	@Transactional
 	public Usuario salvar(Usuario usuario) {
 		try {			
+			usuario.setPassword(passwordEncoder.encode(usuario.getPassword()));
 			return usuarioRepository.save(usuario);
 		} catch (DataIntegrityViolationException e) {
 			throw new UsernameUniqueViolationException(String.format("Username {%s} ja cadastrado", usuario.getUsername()));
@@ -42,12 +46,13 @@ public class UsuarioService {
 		if(!novaSenha.equals(comfirmarSenha)) {
 			throw new PasswordInvalidException("Nova senha não confere com confirmação de senha");
 		}
+		
 		Usuario user = buscarPorId(id);
-		if(!user.getPassword().equals(senhaAtual)) {
+		if(!passwordEncoder.matches(senhaAtual, user.getPassword())) {
 			throw new PasswordInvalidException("Sua senha nao confere");
 		}
 			
-		user.setPassword(novaSenha);
+		user.setPassword(passwordEncoder.encode(novaSenha));
 		return user;
 	}
 
