52341ef3b465d5203fd2a77e4e85b5cd5cde0913
GeorgeSalu <george.salu10@gmail.com>
2023-09-03 14:27:04 -0300

    projeto park-api - tratando os erros 401(Unauthorized) e 403(Forbidden)

--- park-api/src/main/java/com/example/parkapi/web/exception/ApiExceptionHandler.java
+++ park-api/src/main/java/com/example/parkapi/web/exception/ApiExceptionHandler.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -20,7 +21,18 @@ import jakarta.servlet.http.HttpServletRequest;
 public class ApiExceptionHandler {
 	
 	private static final Logger log = LoggerFactory.getLogger(ApiExceptionHandler.class);
-
+	
+	// excecao resposavel para quanod vc nao tem permissao de acesso a um metodo
+	@ExceptionHandler(AccessDeniedException.class)
+	public ResponseEntity<ErrorMessage> accessDeniedException(AccessDeniedException ex, 
+																		HttpServletRequest request) {
+		log.error("Api Error - "+ex);
+		return ResponseEntity
+					.status(HttpStatus.FORBIDDEN)
+					.contentType(MediaType.APPLICATION_JSON)
+					.body(new ErrorMessage(request, HttpStatus.FORBIDDEN, ex.getMessage()));
+	}
+	
 	@ExceptionHandler(PasswordInvalidException.class)
 	public ResponseEntity<ErrorMessage> passwordInvalidException(RuntimeException ex, 
 																		HttpServletRequest request) {
