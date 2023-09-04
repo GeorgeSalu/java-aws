22de72db346c3b58d0c979a2f29399f3fe230ba1
GeorgeSalu <george.salu10@gmail.com>
2023-09-04 20:08:08 -0300

    projeto park-api - teste de autenticacao com status code 422

--- park-api/src/test/java/com/example/parkapi/AutenticacaoIT.java
+++ park-api/src/test/java/com/example/parkapi/AutenticacaoIT.java
@@ -92,5 +92,47 @@ public class AutenticacaoIT {
        Assertions.assertThat(responseBody).isNotNull();
        Assertions.assertThat(responseBody.getStatus()).isEqualTo(422);
    }
+   
+   @Test
+   public void autenticar_ComPasswordInvalido_RetornarErrorMessageStatus422() {
+       ErrorMessage responseBody = testClient
+               .post()
+               .uri("/api/v1/auth")
+               .contentType(MediaType.APPLICATION_JSON)
+               .bodyValue(new UsuarioLoginDto("ana@email.com", ""))
+               .exchange()
+               .expectStatus().isEqualTo(422)
+               .expectBody(ErrorMessage.class)
+               .returnResult().getResponseBody();
+
+       Assertions.assertThat(responseBody).isNotNull();
+       Assertions.assertThat(responseBody.getStatus()).isEqualTo(422);
+
+       responseBody = testClient
+               .post()
+               .uri("/api/v1/auth")
+               .contentType(MediaType.APPLICATION_JSON)
+               .bodyValue(new UsuarioLoginDto("ana@email.com", "123"))
+               .exchange()
+               .expectStatus().isEqualTo(422)
+               .expectBody(ErrorMessage.class)
+               .returnResult().getResponseBody();
+
+       Assertions.assertThat(responseBody).isNotNull();
+       Assertions.assertThat(responseBody.getStatus()).isEqualTo(422);
+
+       responseBody = testClient
+               .post()
+               .uri("/api/v1/auth")
+               .contentType(MediaType.APPLICATION_JSON)
+               .bodyValue(new UsuarioLoginDto("ana@email.com", "123456789"))
+               .exchange()
+               .expectStatus().isEqualTo(422)
+               .expectBody(ErrorMessage.class)
+               .returnResult().getResponseBody();
+
+       Assertions.assertThat(responseBody).isNotNull();
+       Assertions.assertThat(responseBody.getStatus()).isEqualTo(422);
+   }
 
 }
