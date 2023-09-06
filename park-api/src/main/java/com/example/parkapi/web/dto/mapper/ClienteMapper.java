4190e613139032943f8ab8309cea830de15f90c0
GeorgeSalu <george.salu10@gmail.com>
2023-09-06 17:45:50 -0300

    projeto park-api - dto e mapper para o recurso de cliente

---
+++ park-api/src/main/java/com/example/parkapi/web/dto/mapper/ClienteMapper.java
@@ -0,0 +1,19 @@
+package com.example.parkapi.web.dto.mapper;
+
+import org.modelmapper.ModelMapper;
+
+import com.example.parkapi.entity.Cliente;
+import com.example.parkapi.web.dto.ClienteCreateDto;
+import com.example.parkapi.web.dto.ClienteResponseDto;
+
+public class ClienteMapper {
+
+	public static Cliente toCliente(ClienteCreateDto dto) {
+		return new ModelMapper().map(dto, Cliente.class);
+	}
+	
+	public static ClienteResponseDto todto(Cliente cliente) {
+		return new ModelMapper().map(cliente, ClienteResponseDto.class);
+	}
+	
+}
