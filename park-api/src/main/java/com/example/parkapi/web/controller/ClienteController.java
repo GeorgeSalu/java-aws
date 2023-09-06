8a34b1407836aa5879d03a3ef26be8d1f1f05c4e
GeorgeSalu <george.salu10@gmail.com>
2023-09-06 18:45:49 -0300

    projeto park-api - controllador para cliente e o recurso create

---
+++ park-api/src/main/java/com/example/parkapi/web/controller/ClienteController.java
@@ -0,0 +1,45 @@
+package com.example.parkapi.web.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.parkapi.entity.Cliente;
+import com.example.parkapi.jwt.JwtUserDetails;
+import com.example.parkapi.service.ClienteService;
+import com.example.parkapi.service.UsuarioService;
+import com.example.parkapi.web.dto.ClienteCreateDto;
+import com.example.parkapi.web.dto.ClienteResponseDto;
+import com.example.parkapi.web.dto.mapper.ClienteMapper;
+
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("api/v1/cliente")
+public class ClienteController {
+
+	private final ClienteService clienteService;
+	private final UsuarioService usuarioService;
+
+	public ClienteController(ClienteService clienteService, UsuarioService usuarioService) {
+		this.clienteService = clienteService;
+		this.usuarioService = usuarioService;
+	}
+
+
+	@PostMapping
+	@PreAuthorize("hasRole('CLIENTE')")
+	public ResponseEntity<ClienteResponseDto> create(@RequestBody @Valid ClienteCreateDto dto,
+													@AuthenticationPrincipal JwtUserDetails userDetails) {
+		Cliente cliente = ClienteMapper.toCliente(dto);
+		cliente.setUsuario(usuarioService.buscarPorId(userDetails.getId()));
+		clienteService.salvar(cliente);
+		return ResponseEntity.status(201).body(ClienteMapper.todto(cliente));
+	}
+	
+}
