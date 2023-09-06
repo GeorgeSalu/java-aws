18f15a03fd1f7f9c3370d75765e21e4a7d13d305
GeorgeSalu <george.salu10@gmail.com>
2023-09-06 19:14:19 -0300

    projeto park-api - teste ponto a ponto para create(status 201)

---
+++ park-api/src/test/java/com/example/parkapi/ClienteIT.java
@@ -0,0 +1,41 @@
+package com.example.parkapi;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import com.example.parkapi.web.dto.ClienteCreateDto;
+import com.example.parkapi.web.dto.ClienteResponseDto;
+
+@Sql(scripts = "/sql/clientes/clientes-insert.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(scripts = "/sql/clientes/clientes-delete.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ClienteIT {
+
+	@Autowired
+	WebTestClient testClient;
+	
+    @Test
+    public void criarCliente_ComDadosValidos_RetornarClienteComStatus201() {
+        ClienteResponseDto responseBody = testClient
+                .post()
+                .uri("/api/v1/clientes")
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient, "toby@email.com", "123456"))
+                .bodyValue(new ClienteCreateDto("Tobias Ferreira", "91191064085"))
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody(ClienteResponseDto.class)
+                .returnResult().getResponseBody();
+
+        Assertions.assertThat(responseBody).isNotNull();
+        Assertions.assertThat(responseBody.getId()).isNotNull();
+        Assertions.assertThat(responseBody.getNome()).isEqualTo("Tobias Ferreira");
+        Assertions.assertThat(responseBody.getCpf()).isEqualTo("91191064085");
+    }
+	
+}
