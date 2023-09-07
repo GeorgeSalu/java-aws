34c6676c001673356cdab0cc0ca2233fc79c0f88
GeorgeSalu <george.salu10@gmail.com>
2023-09-07 19:04:02 -0300

    projeto park-api - documentando o recurso de criacao de clientes

--- park-api/src/main/java/com/example/parkapi/web/controller/ClienteController.java
+++ park-api/src/main/java/com/example/parkapi/web/controller/ClienteController.java
@@ -2,6 +2,7 @@ package com.example.parkapi.web.controller;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -22,9 +23,14 @@ import com.example.parkapi.web.dto.ClienteResponseDto;
 import com.example.parkapi.web.dto.PageableDto;
 import com.example.parkapi.web.dto.mapper.ClienteMapper;
 import com.example.parkapi.web.dto.mapper.PageableMapper;
-import io.swagger.v3.oas.annotations.Parameter;
-import org.springframework.data.web.PageableDefault;
+import com.example.parkapi.web.exception.ErrorMessage;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 
 @RestController
@@ -39,7 +45,20 @@ public class ClienteController {
 		this.usuarioService = usuarioService;
 	}
 
-
+    @Operation(summary = "Criar um novo cliente",
+            description = "Recurso para criar um novo cliente vinculado a um usuário cadastrado. " +
+                    "Requisição exige uso de um bearer token. Acesso restrito a Role='CLIENTE'",
+            security = @SecurityRequirement(name = "security"),
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Recurso criado com sucesso",
+                            content = @Content(mediaType = " application/json;charset=UTF-8", schema = @Schema(implementation = ClienteResponseDto.class))),
+                    @ApiResponse(responseCode = "409", description = "Cliente CPF já possui cadastro no sistema",
+                            content = @Content(mediaType = " application/json;charset=UTF-8", schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "422", description = "Recurso não processado por falta de dados ou dados inválidos",
+                            content = @Content(mediaType = " application/json;charset=UTF-8", schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "403", description = "Recurso não permito ao perfil de ADMIN",
+                            content = @Content(mediaType = " application/json;charset=UTF-8", schema = @Schema(implementation = ErrorMessage.class)))
+            })
 	@PostMapping
 	@PreAuthorize("hasRole('CLIENTE')")
 	public ResponseEntity<ClienteResponseDto> create(@RequestBody @Valid ClienteCreateDto dto,
