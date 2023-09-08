d62b6d8ba071d9c6b481c6c46334c85a4cfb06cc
GeorgeSalu <george.salu10@gmail.com>
2023-09-07 22:16:28 -0300

    projeto park-api - criando o mapeamento a entiadde Vaga e o repositorio

---
+++ park-api/src/main/java/com/example/parkapi/repository/VagaRepository.java
@@ -0,0 +1,9 @@
+package com.example.parkapi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.parkapi.entity.Vaga;
+
+public interface VagaRepository extends JpaRepository<Vaga, Long>{
+
+}
