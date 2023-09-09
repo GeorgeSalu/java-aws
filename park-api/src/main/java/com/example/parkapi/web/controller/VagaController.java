8f5e417a8ed5ec1d7d616442bc8ac49aebb884dd
GeorgeSalu <george.salu10@gmail.com>
2023-09-09 14:04:32 -0300

    projeto park-api - documentando o recurso create vagas

--- park-api/src/main/java/com/example/parkapi/web/controller/VagaController.java
+++ park-api/src/main/java/com/example/parkapi/web/controller/VagaController.java
@@ -2,6 +2,7 @@ package com.example.parkapi.web.controller;
 
 import java.net.URI;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,7 +18,14 @@ import com.example.parkapi.service.VagaService;
 import com.example.parkapi.web.dto.VagaCreateDto;
 import com.example.parkapi.web.dto.VagaResponseDto;
 import com.example.parkapi.web.dto.mapper.VagaMapper;
+import com.example.parkapi.web.exception.ErrorMessage;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.headers.Header;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 
 @RestController
@@ -30,6 +38,23 @@ public class VagaController {
 		this.vagaService = vagaService;
 	}
 	
+    @Operation(summary = "Criar uma nova vaga", description = "Recurso para criar uma nova vaga." +
+            "Requisição exige uso de um bearer token. Acesso restrito a Role='ADMIN'",
+            security = @SecurityRequirement(name = "security"),
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Recurso criado com sucesso",
+                            headers = @Header(name = HttpHeaders.LOCATION, description = "URL do recurso criado")),
+                    @ApiResponse(responseCode = "409", description = "Vaga já cadastrada",
+                            content = @Content(mediaType = " application/json;charset=UTF-8",
+                                    schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "422", description = "Recurso não processado por falta de dados ou dados inválidos",
+                            content = @Content(mediaType = " application/json;charset=UTF-8",
+                                    schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "403", description = "Recurso não permito ao perfil de CLIENTE",
+                            content = @Content(mediaType = " application/json;charset=UTF-8",
+                                    schema = @Schema(implementation = ErrorMessage.class))
+                    )
+            })
 	@PostMapping
 	@PreAuthorize("hasRole('ADMIN')")
 	public ResponseEntity<Void> create(@RequestBody @Valid VagaCreateDto dto) {
