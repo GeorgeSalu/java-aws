70c688c8d2aedf0618cc578ffc76a402ffa8ce86
GeorgeSalu <george.salu10@gmail.com>
2023-09-09 11:24:20 -0300

    projeto park-api - incluindo serialVersionUID na classe CodigoUniqueViolationException

--- park-api/src/main/java/com/example/parkapi/exception/CodigoUniqueViolationException.java
+++ park-api/src/main/java/com/example/parkapi/exception/CodigoUniqueViolationException.java
@@ -2,6 +2,8 @@ package com.example.parkapi.exception;
 
 public class CodigoUniqueViolationException extends RuntimeException {
 
+	private static final long serialVersionUID = 4410385608788306900L;
+
 	public CodigoUniqueViolationException(String message) {
 		super(message);
 	}
