a90a03ddadff09f97e2fc8c92cff87f2a94638b0
GeorgeSalu <george.salu10@gmail.com>
2023-09-11 20:37:37 -0300

    projeto park-api - documenta o metodo check-in em EstacionamentoController

--- park-api/src/main/java/com/example/parkapi/web/controller/EstacionamentoController.java
+++ park-api/src/main/java/com/example/parkapi/web/controller/EstacionamentoController.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -28,8 +29,15 @@ import com.example.parkapi.web.dto.EstacionamentoResponseDto;
 import com.example.parkapi.web.dto.PageableDto;
 import com.example.parkapi.web.dto.mapper.ClienteVagaMapper;
 import com.example.parkapi.web.dto.mapper.PageableMapper;
+import com.example.parkapi.web.exception.ErrorMessage;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.headers.Header;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 
 @RestController
@@ -45,6 +53,26 @@ public class EstacionamentoController {
 		this.clienteVagaService = clienteVagaService;
 	}
 
+    @Operation(summary = "Operação de check-in", description = "Recurso para dar entrada de um veículo no estacionamento. " +
+            "Requisição exige uso de um bearer token. Acesso restrito a Role='ADMIN'",
+            security = @SecurityRequirement(name = "security"),
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Recurso criado com sucesso",
+                            headers = @Header(name = HttpHeaders.LOCATION, description = "URL de acesso ao recurso criado"),
+                            content = @Content(mediaType = " application/json;charset=UTF-8",
+                                    schema = @Schema(implementation = EstacionamentoResponseDto.class))),
+                    @ApiResponse(responseCode = "404", description = "Causas possiveis: <br/>" +
+                            "- CPF do cliente não cadastrado no sistema; <br/>" +
+                            "- Nenhuma vaga livre foi localizada;",
+                            content = @Content(mediaType = " application/json;charset=UTF-8",
+                                    schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "422", description = "Recurso não processado por falta de dados ou dados inválidos",
+                            content = @Content(mediaType = " application/json;charset=UTF-8",
+                                    schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "403", description = "Recurso não permito ao perfil de CLIENTE",
+                            content = @Content(mediaType = " application/json;charset=UTF-8",
+                                    schema = @Schema(implementation = ErrorMessage.class)))
+            })
 	@PostMapping("/check-in")
 	@PreAuthorize("hasRole('ADMIN')")
 	public ResponseEntity<EstacionamentoResponseDto> checkin(@RequestBody @Valid EstacionamentoCreateDto dto) {
