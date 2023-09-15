1fbdfa51a6aec9a3884dd7eb5eeee553c05c6242
GeorgeSalu <george.salu10@gmail.com>
2023-09-14 22:35:07 -0300

    especialista jpa - inserindo o primeiro objeto com persist

--- especialista-jpa/src/test/java/com/algaworks/ecommerce/iniciandocomjpa/OperacoesComTransacaoTest.java
+++ especialista-jpa/src/test/java/com/algaworks/ecommerce/iniciandocomjpa/OperacoesComTransacaoTest.java
@@ -1,11 +1,37 @@
 package com.algaworks.ecommerce.iniciandocomjpa;
 
+import java.math.BigDecimal;
+
+import org.junit.Assert;
 import org.junit.Test;
 
 import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Produto;
 
 public class OperacoesComTransacaoTest extends EntityManagerTest {
 
+	@Test
+	public void inserindoOPrimeiroObjeto() {
+		Produto produto = new Produto();
+		
+		produto.setId(2);
+		produto.setNome("Camera canon");
+		produto.setDescricao("A melhor definicao para suas fotos");
+		produto.setPreco(new BigDecimal(5000));
+		
+		entityManager.getTransaction().begin();
+		entityManager.persist(produto);
+		entityManager.getTransaction().commit();
+
+		// limpa o entity manager fazendo com que a a proxima operação de find ja no banco de dados 
+		// sem o "entityManager.clear()" ele pega o que esta na memoria
+		entityManager.clear();
+	
+		Produto produtoVerificacao = entityManager.find(Produto.class, produto.getId());
+		Assert.assertNotNull(produtoVerificacao);
+	}
+	
+	
 	@Test
 	public void abrirEFecharUmaTransacao() {
 		entityManager.getTransaction().begin();
