d3ad87c56b134fad2dad67f307758c17cca9bcdc
GeorgeSalu <george.salu10@gmail.com>
2023-09-16 17:47:05 -0300

    especialista jpa - estrategia para chave primaria com identity

--- especialista-jpa/src/main/java/com/algaworks/ecommerce/model/Categoria.java
+++ especialista-jpa/src/main/java/com/algaworks/ecommerce/model/Categoria.java
@@ -6,7 +6,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.persistence.TableGenerator;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -21,14 +20,7 @@ public class Categoria {
 
 	@EqualsAndHashCode.Include
 	@Id
-	@GeneratedValue(strategy = GenerationType.TABLE, generator = "tabela")
-	@TableGenerator(name = "tabela", 
-					table = "hibernate_sequences", 
-					pkColumnName = "sequence_name", 
-					pkColumnValue = "categorias", 
-					valueColumnName = "next_val",
-					initialValue = 0,
-					allocationSize = 50)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
