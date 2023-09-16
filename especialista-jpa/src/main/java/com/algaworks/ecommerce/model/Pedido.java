7fe76b974ffe261a002e8f9805297a1a3ff9b346
GeorgeSalu <george.salu10@gmail.com>
2023-09-16 16:47:46 -0300

    especialista jpa - corrige nome de coluna na classe Pedido

--- especialista-jpa/src/main/java/com/algaworks/ecommerce/model/Pedido.java
+++ especialista-jpa/src/main/java/com/algaworks/ecommerce/model/Pedido.java
@@ -26,7 +26,7 @@ public class Pedido {
 	@Column(name = "data_pedido")
 	private LocalDateTime dataPedido;
 	
-	@Column(name = "data_conclusaoo")
+	@Column(name = "data_conclusao")
 	private LocalDateTime dataConclusao;
 	
 	@Column(name = "nota_fiscal_id")
