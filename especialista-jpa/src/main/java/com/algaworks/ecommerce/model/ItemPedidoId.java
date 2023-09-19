5e1a8e3181cba661ebd747b09ea91a5563900018
GeorgeSalu <george.salu10@gmail.com>
2023-09-19 20:12:41 -0300

    especialista jpa - mapeando chave composta com @IdClass

---
+++ especialista-jpa/src/main/java/com/algaworks/ecommerce/model/ItemPedidoId.java
@@ -0,0 +1,24 @@
+package com.algaworks.ecommerce.model;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public class ItemPedidoId implements Serializable {
+
+	@EqualsAndHashCode.Include
+	private Integer pedidoId;
+	
+	@EqualsAndHashCode.Include
+	private Integer produtoId;
+	
+}
