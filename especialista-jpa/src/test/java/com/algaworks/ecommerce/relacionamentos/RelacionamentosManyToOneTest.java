ab20e8c04c582a39fba2e35d15ecfeb5e4fd6a23
GeorgeSalu <george.salu10@gmail.com>
2023-09-19 20:22:03 -0300

    especialista jpa - exercicio idClass

--- especialista-jpa/src/test/java/com/algaworks/ecommerce/relacionamentos/RelacionamentosManyToOneTest.java
+++ especialista-jpa/src/test/java/com/algaworks/ecommerce/relacionamentos/RelacionamentosManyToOneTest.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 import com.algaworks.ecommerce.EntityManagerTest;
 import com.algaworks.ecommerce.model.Cliente;
 import com.algaworks.ecommerce.model.ItemPedido;
+import com.algaworks.ecommerce.model.ItemPedidoId;
 import com.algaworks.ecommerce.model.Pedido;
 import com.algaworks.ecommerce.model.Produto;
 import com.algaworks.ecommerce.model.StatusPedido;
@@ -18,51 +19,62 @@ public class RelacionamentosManyToOneTest extends EntityManagerTest {
 	@Test
 	public void verificarRelacionamento() {
 		Cliente cliente = entityManager.find(Cliente.class, 1);
-		
+
 		Pedido pedido = new Pedido();
 		pedido.setStatus(StatusPedido.AGUARDANDO);
 		pedido.setDataCriacao(LocalDateTime.now());
 		pedido.setTotal(BigDecimal.TEN);
 
 		pedido.setCliente(cliente);
-		
+
 		entityManager.getTransaction().begin();
 		entityManager.persist(pedido);
 		entityManager.getTransaction().commit();
-		
+
 		entityManager.clear();
-		
+
 		Pedido pedidoVerificacao = entityManager.find(Pedido.class, pedido.getId());
 		Assert.assertNotNull(pedidoVerificacao);
 	}
-	
-    @Test
-    public void verificarRelacionamentoItemPedido() {
-        Cliente cliente = entityManager.find(Cliente.class, 1);
-        Produto produto = entityManager.find(Produto.class, 1);
 
-        Pedido pedido = new Pedido();
-        pedido.setStatus(StatusPedido.AGUARDANDO);
-        pedido.setDataCriacao(LocalDateTime.now());
-        pedido.setTotal(BigDecimal.TEN);
-        pedido.setCliente(cliente);
+	@Test
+	public void verificarRelacionamentoItemPedido() {
+		entityManager.getTransaction().begin();
 
-        ItemPedido itemPedido = new ItemPedido();
-        itemPedido.setPrecoProduto(produto.getPreco());
-        itemPedido.setQuantidade(1);
-        itemPedido.setPedido(pedido);
-        itemPedido.setProduto(produto);
+		Cliente cliente = entityManager.find(Cliente.class, 1);
+		Produto produto = entityManager.find(Produto.class, 1);
 
-        entityManager.getTransaction().begin();
-        entityManager.persist(pedido);
-        entityManager.persist(itemPedido);
-        entityManager.getTransaction().commit();
+		Pedido pedido = new Pedido();
+		pedido.setStatus(StatusPedido.AGUARDANDO);
+		pedido.setDataCriacao(LocalDateTime.now());
+		pedido.setTotal(BigDecimal.TEN);
+		pedido.setCliente(cliente);
 
-        entityManager.clear();
+		entityManager.persist(pedido);
 
-        ItemPedido itemPedidoVerificacao = entityManager.find(ItemPedido.class, itemPedido.getId());
-        Assert.assertNotNull(itemPedidoVerificacao.getPedido());
-        Assert.assertNotNull(itemPedidoVerificacao.getProduto());
-    }
-	
+		// Pode ser que logo ao executar o método "persist" o JPA já faça a
+		// sincronização com a base.
+		// Mas caso isso não aconteça, o flush garante a sincronização.
+		entityManager.flush();
+
+		ItemPedido itemPedido = new ItemPedido();
+		itemPedido.setPedidoId(pedido.getId());
+		itemPedido.setProdutoId(produto.getId());
+		itemPedido.setPrecoProduto(produto.getPreco());
+		itemPedido.setQuantidade(1);
+		itemPedido.setPedido(pedido);
+		itemPedido.setProduto(produto);
+
+		entityManager.persist(itemPedido);
+
+		entityManager.getTransaction().commit();
+
+		entityManager.clear();
+
+		ItemPedido itemPedidoVerificacao = entityManager.find(ItemPedido.class,
+				new ItemPedidoId(pedido.getId(), produto.getId()));
+		Assert.assertNotNull(itemPedidoVerificacao.getPedido());
+		Assert.assertNotNull(itemPedidoVerificacao.getProduto());
+	}
+
 }
