4a0e455c2aed96e63eb730874c432b7562b32361
GeorgeSalu <george.salu10@gmail.com>
2023-09-18 21:06:09 -0300

    especialista jpa - exercicio optional

--- especialista-jpa/src/test/java/com/algaworks/ecommerce/mapeamentobasico/MapeamentoObjetoEmbutidoTest.java
+++ especialista-jpa/src/test/java/com/algaworks/ecommerce/mapeamentobasico/MapeamentoObjetoEmbutidoTest.java
@@ -7,6 +7,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Cliente;
 import com.algaworks.ecommerce.model.EnderecoEntregaPedido;
 import com.algaworks.ecommerce.model.Pedido;
 import com.algaworks.ecommerce.model.StatusPedido;
@@ -15,30 +16,34 @@ public class MapeamentoObjetoEmbutidoTest extends EntityManagerTest {
 
 	@Test
 	public void analisarMapeamentoObjetoEmbutido() {
+		Cliente cliente = entityManager.find(Cliente.class, 1);
+
 		EnderecoEntregaPedido endereco = new EnderecoEntregaPedido();
-		endereco.setCep("000-112");
-		endereco.setLogradouro("Rua das laranjeiras");
-		endereco.setNumero("1212");
-		endereco.setBairro("novo");
-		endereco.setCidade("nova");
-		
-		
+		endereco.setCep("00000-00");
+		endereco.setLogradouro("Rua das Laranjeiras");
+		endereco.setNumero("123");
+		endereco.setBairro("Centro");
+		endereco.setCidade("Uberlândia");
+		endereco.setEstado("MG");
+
 		Pedido pedido = new Pedido();
-		// pedido.setId(1); comentando por estamos utilizando IDENTITY
+		// pedido.setId(1); Comentado porque estamos utilizando IDENTITY
 		pedido.setDataPedido(LocalDateTime.now());
 		pedido.setStatus(StatusPedido.AGUARDANDO);
-		pedido.setTotal(new BigDecimal(10000));
+		pedido.setTotal(new BigDecimal(1000));
 		pedido.setEnderecoEntrega(endereco);
-		
+		pedido.setCliente(cliente);
+
 		entityManager.getTransaction().begin();
 		entityManager.persist(pedido);
 		entityManager.getTransaction().commit();
-		
+
 		entityManager.clear();
-		
+
 		Pedido pedidoVerificacao = entityManager.find(Pedido.class, pedido.getId());
 		Assert.assertNotNull(pedidoVerificacao);
+		Assert.assertNotNull(pedidoVerificacao.getEnderecoEntrega());
 		Assert.assertNotNull(pedidoVerificacao.getEnderecoEntrega().getCep());
 	}
-	
+
 }
