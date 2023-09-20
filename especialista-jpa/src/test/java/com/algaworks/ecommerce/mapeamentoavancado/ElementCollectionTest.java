f9665c6980dae20e607659bb02f45dad1893f492
GeorgeSalu <george.salu10@gmail.com>
2023-09-20 19:32:09 -0300

    especialista jpa - mapenando cloecoes de objetos embutidos com @ElementCollection

--- especialista-jpa/src/test/java/com/algaworks/ecommerce/mapeamentoavancado/ElementCollectionTest.java
+++ especialista-jpa/src/test/java/com/algaworks/ecommerce/mapeamentoavancado/ElementCollectionTest.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import org.junit.Test;
 
 import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Atributo;
 import com.algaworks.ecommerce.model.Produto;
 
 import junit.framework.Assert;
@@ -26,4 +27,19 @@ public class ElementCollectionTest extends EntityManagerTest {
 		Assert.assertFalse(produtoVerificacao.getTags().isEmpty());
 	}
 
+	@Test
+    public void aplicarAtributos() {
+        entityManager.getTransaction().begin();
+
+        Produto produto = entityManager.find(Produto.class, 1);
+        produto.setAtributos(Arrays.asList(new Atributo("tela", "320x600")));
+
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Produto produtoVerificacao = entityManager.find(Produto.class, produto.getId());
+        Assert.assertFalse(produtoVerificacao.getAtributos().isEmpty());
+    }
+	
 }
