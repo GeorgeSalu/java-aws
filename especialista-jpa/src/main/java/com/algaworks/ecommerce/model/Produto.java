51b5c434391f5f93aa707e1e5f65d6c8dc20c8da
GeorgeSalu <george.salu10@gmail.com>
2023-09-20 18:47:30 -0300

    especialista jpa - colecoes de tipos basicos

--- especialista-jpa/src/main/java/com/algaworks/ecommerce/model/Produto.java
+++ especialista-jpa/src/main/java/com/algaworks/ecommerce/model/Produto.java
@@ -4,13 +4,16 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import javax.persistence.CollectionTable;
 import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinColumns;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToOne;
@@ -55,4 +58,11 @@ public class Produto {
 
 	@OneToOne(mappedBy = "produto")
 	private Estoque estoque;
+	
+	@ElementCollection
+	@CollectionTable(name = "produto_tag", joinColumns = @JoinColumn(name = "produto_id"))
+	@Column(name = "tag")
+	private List<String> tags;
 }
+
+
