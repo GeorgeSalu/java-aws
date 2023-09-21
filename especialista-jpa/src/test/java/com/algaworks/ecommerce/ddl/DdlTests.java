61a8109c9c968c960002c845e886dcd8a2013a36
GeorgeSalu <george.salu10@gmail.com>
2023-09-21 20:10:31 -0300

    especialista jpa - table name schema catalog uniquecontraints e indexs

---
+++ especialista-jpa/src/test/java/com/algaworks/ecommerce/ddl/DdlTests.java
@@ -0,0 +1,14 @@
+package com.algaworks.ecommerce.ddl;
+
+import org.junit.Test;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+
+public class DdlTests extends EntityManagerTest {
+
+	@Test
+	public void gerarDDL() {
+		// teste vazio... somente para exibir o ddl
+	}
+	
+}
