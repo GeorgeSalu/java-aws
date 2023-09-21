5bf493d4c0f10ef9d059b2bdd808a065d4a152e7
GeorgeSalu <george.salu10@gmail.com>
2023-09-21 18:00:28 -0300

    especialista jpa - salvar foto do produto

--- especialista-jpa/src/test/java/com/algaworks/ecommerce/mapeamentoavancado/SalvandoArquivosTest.java
+++ especialista-jpa/src/test/java/com/algaworks/ecommerce/mapeamentoavancado/SalvandoArquivosTest.java
@@ -8,11 +8,26 @@ import org.junit.Test;
 import com.algaworks.ecommerce.EntityManagerTest;
 import com.algaworks.ecommerce.model.NotaFiscal;
 import com.algaworks.ecommerce.model.Pedido;
+import com.algaworks.ecommerce.model.Produto;
 
 import junit.framework.Assert;
 
 public class SalvandoArquivosTest extends EntityManagerTest {
 
+    @Test
+    public void salvarFotoProduto(){
+        entityManager.getTransaction().begin();
+        Produto produto = entityManager.find(Produto.class, 1);
+        produto.setFoto(carregarFoto());
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Produto produtoVerificacao = entityManager.find(Produto.class, 1);
+        Assert.assertNotNull(produtoVerificacao.getFoto());
+        Assert.assertTrue(produtoVerificacao.getFoto().length > 0);
+    }
+	
     @Test
     public void salvarXmlNota() {
         Pedido pedido = entityManager.find(Pedido.class, 1);
@@ -41,4 +56,16 @@ public class SalvandoArquivosTest extends EntityManagerTest {
         }
     }
 	
+    private static byte[] carregarFoto() {
+        return carregarArquivo("/kindle.jpg");
+    }
+
+    private static byte[] carregarArquivo(String nome) {
+        try {
+            return SalvandoArquivosTest.class.getResourceAsStream(nome).readAllBytes();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    
 }
