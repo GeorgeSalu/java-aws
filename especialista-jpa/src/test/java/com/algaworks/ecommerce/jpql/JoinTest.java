01020af66de65705b427b3401627202d8c261d44
GeorgeSalu <george.salu10@gmail.com>
2023-09-23 16:24:30 -0300

    especialista jpa - usando left outer join

--- especialista-jpa/src/test/java/com/algaworks/ecommerce/jpql/JoinTest.java
+++ especialista-jpa/src/test/java/com/algaworks/ecommerce/jpql/JoinTest.java
@@ -12,6 +12,16 @@ import com.algaworks.ecommerce.EntityManagerTest;
 public class JoinTest extends EntityManagerTest {
 
 	@Test
+	public void fazerLeftJoin() {
+		String jpql = "select p from Pedido p left join p.pagamento pag on pag.status = 'PROCESSANDO'";
+		
+		TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+		
+		List<Object[]> lista = typedQuery.getResultList();
+		Assert.assertFalse(lista.isEmpty());
+	}
+	
+	@Test
 	public void fazerJoin() {
 		String jpql = "select p from Pedido p join p.pagamento pag";
 		
