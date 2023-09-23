064c5b8cfcf7adb71504e4798038e7ee7f907064
GeorgeSalu <george.salu10@gmail.com>
2023-09-23 10:43:02 -0300

    especialista jpa - joincolumn name nullable foreingkey unique e columnDefinition

--- especialista-jpa/src/main/java/com/algaworks/ecommerce/model/NotaFiscal.java
+++ especialista-jpa/src/main/java/com/algaworks/ecommerce/model/NotaFiscal.java
@@ -4,6 +4,7 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
@@ -28,7 +29,8 @@ public class NotaFiscal  {
 
 	@MapsId
 	@OneToOne(optional = false)
-	@JoinColumn(name = "pedido_id")
+	@JoinColumn(name = "pedido_id", nullable = false,
+				foreignKey = @ForeignKey(name = "fk_nota_fiscal_pedido"))
 	private Pedido pedido;
 	
 	@Column(nullable = false)
