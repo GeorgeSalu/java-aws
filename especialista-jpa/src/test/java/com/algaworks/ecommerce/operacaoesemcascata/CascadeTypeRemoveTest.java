c5baa2cb038c61970fc2362b9f23945ba4e98994
GeorgeSalu <george.salu10@gmail.com>
2023-09-23 14:44:31 -0300

    especialista jpa - remocao em cascata de manytomany

--- especialista-jpa/src/test/java/com/algaworks/ecommerce/operacaoesemcascata/CascadeTypeRemoveTest.java
+++ especialista-jpa/src/test/java/com/algaworks/ecommerce/operacaoesemcascata/CascadeTypeRemoveTest.java
@@ -1,14 +1,32 @@
 package com.algaworks.ecommerce.operacaoesemcascata;
 
 import org.junit.Assert;
+import org.junit.Test;
 
 import com.algaworks.ecommerce.EntityManagerTest;
 import com.algaworks.ecommerce.model.ItemPedido;
 import com.algaworks.ecommerce.model.ItemPedidoId;
 import com.algaworks.ecommerce.model.Pedido;
+import com.algaworks.ecommerce.model.Produto;
 
 public class CascadeTypeRemoveTest extends EntityManagerTest {
 
+    @Test
+    public void removerRelacaoProdutoCategoria() {
+        Produto produto = entityManager.find(Produto.class, 1);
+
+        Assert.assertFalse(produto.getCategorias().isEmpty());
+
+        entityManager.getTransaction().begin();
+        produto.getCategorias().clear();
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Produto produtoVerificacao = entityManager.find(Produto.class, produto.getId());
+        Assert.assertTrue(produtoVerificacao.getCategorias().isEmpty());
+    }
+	
     // @Test
     public void removerPedidoEItens() {
         Pedido pedido = entityManager.find(Pedido.class, 1);
