55ffeb91f36807c1634dc77d46a7c8b4300e6bb4
GeorgeSalu <george.salu10@gmail.com>
2023-09-28 18:35:47 -0300

    especialista jpa - funcoes para datas

--- especialista-jpa/src/test/java/com/algaworks/ecommerce/jpql/FuncoesTest.java
+++ especialista-jpa/src/test/java/com/algaworks/ecommerce/jpql/FuncoesTest.java
@@ -12,7 +12,24 @@ import com.algaworks.ecommerce.EntityManagerTest;
 public class FuncoesTest extends EntityManagerTest {
 
 	@Test
-	public void paginarResultados() {
+	public void aplicarFuncoesData() {
+		//TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+		//year(p.dataCriacao), month(p.dataCricao), day(p.dataCricao)
+		
+		String jpql = "select current_date, current_time, current_timestamp from Pedido p "
+				+ " where p.dataCriacao < current_date";
+		
+		TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+		
+		
+		List<Object[]> lista = typedQuery.getResultList();
+		Assert.assertFalse(lista.isEmpty());
+		
+		lista.forEach(arr -> System.out.println(arr[0] + " - "+ arr[1] + " - "+arr[2]));
+	}
+	
+	@Test
+	public void aplicarFuncoesStrings() {
 		// concat, length, locate, subsctring, lower, upper, trim
 		String jpql = "select c.nome, length(c.nome) from Categoria c";
 		
