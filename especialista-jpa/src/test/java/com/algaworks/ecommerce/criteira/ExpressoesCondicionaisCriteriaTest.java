03d98486180f0438a86bff560b64c92973053419
GeorgeSalu <george.salu10@gmail.com>
2023-10-01 11:34:28 -0300

    especialista jpa - criteria api - usando as expressoes condicionais is null e is empty

--- especialista-jpa/src/test/java/com/algaworks/ecommerce/criteira/ExpressoesCondicionaisCriteriaTest.java
+++ especialista-jpa/src/test/java/com/algaworks/ecommerce/criteira/ExpressoesCondicionaisCriteriaTest.java
@@ -13,9 +13,42 @@ import org.junit.Test;
 import com.algaworks.ecommerce.EntityManagerTest;
 import com.algaworks.ecommerce.model.Cliente;
 import com.algaworks.ecommerce.model.Cliente_;
+import com.algaworks.ecommerce.model.Produto;
+import com.algaworks.ecommerce.model.Produto_;
 
 public class ExpressoesCondicionaisCriteriaTest extends EntityManagerTest {
 
+    @Test
+    public void usarIsEmpty() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Produto> criteriaQuery = criteriaBuilder.createQuery(Produto.class);
+        Root<Produto> root = criteriaQuery.from(Produto.class);
+
+        criteriaQuery.select(root);
+
+        criteriaQuery.where(criteriaBuilder.isEmpty(root.get(Produto_.categorias)));
+
+        TypedQuery<Produto> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Produto> lista = typedQuery.getResultList();
+        Assert.assertTrue(lista.isEmpty());
+    }
+
+    @Test
+    public void usarIsNull() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Produto> criteriaQuery = criteriaBuilder.createQuery(Produto.class);
+        Root<Produto> root = criteriaQuery.from(Produto.class);
+
+        criteriaQuery.select(root);
+
+        criteriaQuery.where(criteriaBuilder.isNull(root.get(Produto_.foto)));
+        // criteriaQuery.where(root.get(Produto_.foto).isNull());
+
+        TypedQuery<Produto> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Produto> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+    }
+	
 	@Test
 	public void usarExpressaoCondicionalLike() {
 		CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
