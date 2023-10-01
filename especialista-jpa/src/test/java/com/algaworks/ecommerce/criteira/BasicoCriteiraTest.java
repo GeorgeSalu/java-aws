dbd16fc9d1e2b311c1941993352e30e14dc4a516
GeorgeSalu <george.salu10@gmail.com>
2023-10-01 08:51:29 -0300

    especialista jpa - criteria api - selecionando o retorno

--- especialista-jpa/src/test/java/com/algaworks/ecommerce/criteira/BasicoCriteiraTest.java
+++ especialista-jpa/src/test/java/com/algaworks/ecommerce/criteira/BasicoCriteiraTest.java
@@ -1,5 +1,7 @@
 package com.algaworks.ecommerce.criteira;
 
+import java.math.BigDecimal;
+
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
@@ -13,6 +15,22 @@ import com.algaworks.ecommerce.model.Pedido;
 
 public class BasicoCriteiraTest extends EntityManagerTest {
 
+	@Test
+	public void selecionarUmAtributoParaRetorno() {
+		CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+		CriteriaQuery<BigDecimal> criteriaQuery = criteriaBuilder.createQuery(BigDecimal.class);
+		Root<Pedido> root = criteriaQuery.from(Pedido.class);
+		
+		criteriaQuery.select(root.get("total"));
+		
+		criteriaQuery.where(criteriaBuilder.equal(root.get("id"), 1));
+		
+		TypedQuery<BigDecimal> typedQuery = entityManager.createQuery(criteriaQuery);
+		BigDecimal total = typedQuery.getSingleResult();
+		
+		Assert.assertEquals(new BigDecimal("2398.00"), total);
+	}
+	
 	@Test
 	public void buscarPorIdentificador() {
 		
