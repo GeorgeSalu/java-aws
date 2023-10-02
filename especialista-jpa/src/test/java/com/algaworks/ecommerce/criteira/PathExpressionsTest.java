02fa8b0cdd85d6491c2620abe13d189b419104f4
GeorgeSalu <george.salu10@gmail.com>
2023-10-02 18:53:41 -0300

    especialista jpa - criteria api - diferenca entre expressions, paths e predicates

---
+++ especialista-jpa/src/test/java/com/algaworks/ecommerce/criteira/PathExpressionsTest.java
@@ -0,0 +1,37 @@
+package com.algaworks.ecommerce.criteira;
+
+import java.util.List;
+
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Cliente_;
+import com.algaworks.ecommerce.model.Pedido;
+import com.algaworks.ecommerce.model.Pedido_;
+
+public class PathExpressionsTest extends EntityManagerTest {
+
+    @Test
+    public void usarPathExpression() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Pedido> criteriaQuery = criteriaBuilder.createQuery(Pedido.class);
+        Root<Pedido> root = criteriaQuery.from(Pedido.class);
+
+        criteriaQuery.select(root);
+
+        criteriaQuery.where(
+                criteriaBuilder.like(root.get(Pedido_.cliente).get(Cliente_.nome), "M%"));
+
+        TypedQuery<Pedido> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Pedido> lista = typedQuery.getResultList();
+
+        Assert.assertFalse(lista.isEmpty());
+    }
+	
+}
