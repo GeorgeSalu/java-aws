d523dffa8b9ad94fb809bfbb82408367aa2652bb
GeorgeSalu <george.salu10@gmail.com>
2023-10-02 19:20:29 -0300

    especialista jpa - criteria api - exercicio buscar pedidos com produto especifico

--- especialista-jpa/src/test/java/com/algaworks/ecommerce/criteira/PathExpressionsTest.java
+++ especialista-jpa/src/test/java/com/algaworks/ecommerce/criteira/PathExpressionsTest.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Join;
 import javax.persistence.criteria.Root;
 
 import org.junit.Assert;
@@ -12,26 +13,45 @@ import org.junit.Test;
 
 import com.algaworks.ecommerce.EntityManagerTest;
 import com.algaworks.ecommerce.model.Cliente_;
+import com.algaworks.ecommerce.model.ItemPedido;
+import com.algaworks.ecommerce.model.ItemPedido_;
 import com.algaworks.ecommerce.model.Pedido;
 import com.algaworks.ecommerce.model.Pedido_;
+import com.algaworks.ecommerce.model.Produto_;
 
 public class PathExpressionsTest extends EntityManagerTest {
 
-    @Test
-    public void usarPathExpression() {
-        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
-        CriteriaQuery<Pedido> criteriaQuery = criteriaBuilder.createQuery(Pedido.class);
-        Root<Pedido> root = criteriaQuery.from(Pedido.class);
+	@Test
+	public void buscarPedidosComProdutoDeIDIgual1Exercicio() {
+		CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+		CriteriaQuery<Pedido> criteriaQuery = criteriaBuilder.createQuery(Pedido.class);
+		Root<Pedido> root = criteriaQuery.from(Pedido.class);
+		Join<Pedido, ItemPedido> joinItemPedido = root.join(Pedido_.itens);
 
-        criteriaQuery.select(root);
+		criteriaQuery.select(root);
 
-        criteriaQuery.where(
-                criteriaBuilder.like(root.get(Pedido_.cliente).get(Cliente_.nome), "M%"));
+		criteriaQuery.where(criteriaBuilder.equal(joinItemPedido.get(ItemPedido_.produto).get(Produto_.id), 1));
 
-        TypedQuery<Pedido> typedQuery = entityManager.createQuery(criteriaQuery);
-        List<Pedido> lista = typedQuery.getResultList();
+		TypedQuery<Pedido> typedQuery = entityManager.createQuery(criteriaQuery);
+		List<Pedido> lista = typedQuery.getResultList();
 
-        Assert.assertFalse(lista.isEmpty());
-    }
-	
+		Assert.assertFalse(lista.isEmpty());
+	}
+
+	@Test
+	public void usarPathExpression() {
+		CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+		CriteriaQuery<Pedido> criteriaQuery = criteriaBuilder.createQuery(Pedido.class);
+		Root<Pedido> root = criteriaQuery.from(Pedido.class);
+
+		criteriaQuery.select(root);
+
+		criteriaQuery.where(criteriaBuilder.like(root.get(Pedido_.cliente).get(Cliente_.nome), "M%"));
+
+		TypedQuery<Pedido> typedQuery = entityManager.createQuery(criteriaQuery);
+		List<Pedido> lista = typedQuery.getResultList();
+
+		Assert.assertFalse(lista.isEmpty());
+	}
+
 }
