14697da60d31478b04ad46bf33dbc322dbbd1abd
GeorgeSalu <george.salu10@gmail.com>
2023-10-03 18:17:39 -0300

    especialista jpa - consultas nativas - executanndo sql e retornando uma entidade

--- especialista-jpa/src/test/java/com/algaworks/ecommerce/consultasNativas/ConsultaNativaTest.java
+++ especialista-jpa/src/test/java/com/algaworks/ecommerce/consultasNativas/ConsultaNativaTest.java
@@ -7,9 +7,22 @@ import javax.persistence.Query;
 import org.junit.Test;
 
 import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Produto;
 
 public class ConsultaNativaTest extends EntityManagerTest {
 
+	@Test
+	public void executarSQLRetornoEntidade() {
+		String sql = "select id, nome, descricao, data_criacao, data_ultima_atualizacao, preco, foto from produto";
+		
+		Query query = entityManager.createNativeQuery(sql, Produto.class);
+		
+		List<Produto> lista = query.getResultList();
+		
+		lista.stream().forEach(obj -> System.out.println(
+				String.format("Produto => ID: %s, Nome: %s", obj.getId(), obj.getNome())));
+	}
+	
 	@Test
 	public void executarSQL() {
 		String sql = "select id, nome from produto";
