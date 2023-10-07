4f442258018bba7bf5be4e35ea698b6803ebe9ac
GeorgeSalu <george.salu10@gmail.com>
2023-10-07 09:57:36 -0300

    automated-tests-with-java - BDD Style - Given, When, Then ou Arrange, Act, Assert

--- automated-tests-with-java/src/test/java/br/com/math/SimpleMathTest.java
+++ automated-tests-with-java/src/test/java/br/com/math/SimpleMathTest.java
@@ -12,13 +12,17 @@ public class SimpleMathTest {
 	@Test
 	@DisplayName("Test 6.2 + 2 = 8.2")
 	void testSum_when_SixDotToenIsAddedByTwo_ShouldReturnEightDotTwo() {
+		// AAA Arrange, Act, Assert
+		// Given / Arrange
 		SimpleMath math = new SimpleMath();
 		double firstNumber = 6.2D;
 		double secondNumber = 2D;
-
-		Double actual = math.sum(firstNumber, secondNumber);
 		double expected = 8.2D;
 
+		// When / Act
+		Double actual = math.sum(firstNumber, secondNumber);
+
+		// Then / Assert
 		assertEquals(expected, actual,
 				() -> firstNumber + " + " + secondNumber + " did not producer " + expected + "!");
 	}
