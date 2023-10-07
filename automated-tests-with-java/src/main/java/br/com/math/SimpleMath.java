868f8b6ca03be05a2463da6677eaf95f07c597eb
GeorgeSalu <george.salu10@gmail.com>
2023-10-06 21:03:59 -0300

    automated-tests-with-java - corrige nome de variaveis em SimpleMath

--- automated-tests-with-java/src/main/java/br/com/math/SimpleMath.java
+++ automated-tests-with-java/src/main/java/br/com/math/SimpleMath.java
@@ -2,24 +2,24 @@ package br.com.math;
 
 public class SimpleMath {
 
-	public Double sum(Double firstName,Double secondNumber) {
-		return firstName + secondNumber;
+	public Double sum(Double firstNumber,Double secondNumber) {
+		return firstNumber + secondNumber;
 	}
 	
-	public Double subtraction(Double firstName,Double secondNumber) {
-		return firstName - secondNumber;
+	public Double subtraction(Double firstNumber,Double secondNumber) {
+		return firstNumber - secondNumber;
 	}
 	
-	public Double multiplication(Double firstName,Double secondNumber) {
-		return firstName * secondNumber;
+	public Double multiplication(Double firstNumber,Double secondNumber) {
+		return firstNumber * secondNumber;
 	}
 	
-	public Double division(Double firstName,Double secondNumber) {
-		return firstName / secondNumber;
+	public Double division(Double firstNumber,Double secondNumber) {
+		return firstNumber / secondNumber;
 	}
 	
-	public Double mean(Double firstName,Double secondNumber) {
-		return (firstName + secondNumber) / 2;
+	public Double mean(Double firstNumber,Double secondNumber) {
+		return (firstNumber + secondNumber) / 2;
 	}
 	
 	public Double squareRoot(Double number) {
