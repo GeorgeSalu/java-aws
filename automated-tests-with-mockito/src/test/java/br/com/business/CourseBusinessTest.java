fe0747f58daed843c6aaedf9d566987efc1fd626
GeorgeSalu <george.salu10@gmail.com>
2023-10-07 14:52:16 -0300

    automated-tests-with-mockito - bdd style

--- automated-tests-with-mockito/src/test/java/br/com/business/CourseBusinessTest.java
+++ automated-tests-with-mockito/src/test/java/br/com/business/CourseBusinessTest.java
@@ -11,23 +11,29 @@ class CourseBusinessTest {
 
 	@Test
 	void testCoursesRelatedToSpring_When_UsingAStub() {
+		// Given / Arrange
 		CourseService stubService = new CourseServiceStub();
 		CourseBusiness business = new CourseBusiness(stubService);
 		
+		// When / Act
 		var filteredCourses = business.retriveCoursesRelatedToSpring("Leandro");
 		
 		
+		// Then / Assert
 		assertEquals(4, filteredCourses.size());
 	}
 
 	@Test
 	void testCoursesRelatedToSpring_When_UsingAFooBarStudent() {
+		// Given / Arrange
 		CourseService stubService = new CourseServiceStub();
 		CourseBusiness business = new CourseBusiness(stubService);
 		
+		// When / Act
 		var filteredCourses = business.retriveCoursesRelatedToSpring("Foo Bar");
 		
 		
+		// Then / Assert
 		assertEquals(0, filteredCourses.size());
 	}
 	
