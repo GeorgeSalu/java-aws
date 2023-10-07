7a93d433839c0b909e02c546858e06efce5074a0
GeorgeSalu <george.salu10@gmail.com>
2023-10-06 22:00:52 -0300

    automated-tests-with-java - formatando SimpleMathTest

--- automated-tests-with-java/src/test/java/br/com/math/SimpleMathTest.java
+++ automated-tests-with-java/src/test/java/br/com/math/SimpleMathTest.java
@@ -10,70 +10,75 @@ public class SimpleMathTest {
 		SimpleMath math = new SimpleMath();
 		double firstNumber = 6.2D;
 		double secondNumber = 2D;
-		
+
 		Double actual = math.sum(firstNumber, secondNumber);
 		double expected = 8.2D;
 
-		assertEquals(expected, actual, () -> firstNumber + " + "+secondNumber+" did not producer "+expected+"!");
+		assertEquals(expected, actual,
+				() -> firstNumber + " + " + secondNumber + " did not producer " + expected + "!");
 	}
-	
+
 	@Test
 	void testSubtraction() {
 		SimpleMath math = new SimpleMath();
 		double firstNumber = 6.2D;
 		double secondNumber = 2D;
-		
+
 		Double actual = math.subtraction(firstNumber, secondNumber);
 		double expected = 4.2D;
 
-		assertEquals(expected, actual, () -> firstNumber + " + "+secondNumber+" did not producer "+expected+"!");
+		assertEquals(expected, actual,
+				() -> firstNumber + " + " + secondNumber + " did not producer " + expected + "!");
 	}
-	
+
 	@Test
 	void testMultiplication() {
 		SimpleMath math = new SimpleMath();
 		double firstNumber = 6.2D;
 		double secondNumber = 2D;
-		
+
 		Double actual = math.multiplication(firstNumber, secondNumber);
 		double expected = 12.4D;
 
-		assertEquals(expected, actual, () -> firstNumber + " + "+secondNumber+" did not producer "+expected+"!");
+		assertEquals(expected, actual,
+				() -> firstNumber + " + " + secondNumber + " did not producer " + expected + "!");
 	}
-	
+
 	@Test
 	void testDivision() {
 		SimpleMath math = new SimpleMath();
 		double firstNumber = 6.2D;
 		double secondNumber = 2D;
-		
+
 		Double actual = math.division(firstNumber, secondNumber);
 		double expected = 3.1D;
 
-		assertEquals(expected, actual, () -> firstNumber + " + "+secondNumber+" did not producer "+expected+"!");
+		assertEquals(expected, actual,
+				() -> firstNumber + " + " + secondNumber + " did not producer " + expected + "!");
 	}
-	
+
 	@Test
 	void testMean() {
 		SimpleMath math = new SimpleMath();
 		double firstNumber = 6.2D;
 		double secondNumber = 2D;
-		
+
 		Double actual = math.mean(firstNumber, secondNumber);
 		double expected = 4.1D;
 
-		assertEquals(expected, actual, () -> "("+firstNumber + " + "+secondNumber+ ")/2"+ " did not producer "+expected+"!");
+		assertEquals(expected, actual,
+				() -> "(" + firstNumber + " + " + secondNumber + ")/2" + " did not producer " + expected + "!");
 	}
-	
+
 	@Test
 	void testSqaureRoot() {
 		SimpleMath math = new SimpleMath();
 		double number = 81D;
-		
+
 		Double actual = math.squareRoot(number);
 		double expected = 9D;
 
-		assertEquals(expected, actual, () -> "Sqaure root of"+ number +" did not producer "+expected+"!");
+		assertEquals(expected, actual, () -> "Sqaure root of" + number + " did not producer " + expected + "!");
 	}
-	
+
 }
