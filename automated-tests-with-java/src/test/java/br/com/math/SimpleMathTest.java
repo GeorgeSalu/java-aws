91847838749faf501d4f6610698acf7438eb9fa9
GeorgeSalu <george.salu10@gmail.com>
2023-10-06 21:27:43 -0300

    automated-tests-with-java - implementando testes automatizados para as demais operações

--- automated-tests-with-java/src/test/java/br/com/math/SimpleMathTest.java
+++ automated-tests-with-java/src/test/java/br/com/math/SimpleMathTest.java
@@ -15,8 +15,65 @@ public class SimpleMathTest {
 		double expected = 8.2D;
 
 		assertEquals(expected, actual, () -> firstNumber + " + "+secondNumber+" did not producer "+expected+"!");
-		assertNotEquals(9.2, actual);
-		assertNotNull(actual);
+	}
+	
+	@Test
+	void testSubtraction() {
+		SimpleMath math = new SimpleMath();
+		double firstNumber = 6.2D;
+		double secondNumber = 2D;
+		
+		Double actual = math.subtraction(firstNumber, secondNumber);
+		double expected = 4.2D;
+
+		assertEquals(expected, actual, () -> firstNumber + " + "+secondNumber+" did not producer "+expected+"!");
+	}
+	
+	@Test
+	void testmultiplication() {
+		SimpleMath math = new SimpleMath();
+		double firstNumber = 6.2D;
+		double secondNumber = 2D;
+		
+		Double actual = math.multiplication(firstNumber, secondNumber);
+		double expected = 12.4D;
+
+		assertEquals(expected, actual, () -> firstNumber + " + "+secondNumber+" did not producer "+expected+"!");
+	}
+	
+	@Test
+	void testDivision() {
+		SimpleMath math = new SimpleMath();
+		double firstNumber = 6.2D;
+		double secondNumber = 2D;
+		
+		Double actual = math.division(firstNumber, secondNumber);
+		double expected = 3.1D;
+
+		assertEquals(expected, actual, () -> firstNumber + " + "+secondNumber+" did not producer "+expected+"!");
+	}
+	
+	@Test
+	void testMean() {
+		SimpleMath math = new SimpleMath();
+		double firstNumber = 6.2D;
+		double secondNumber = 2D;
+		
+		Double actual = math.mean(firstNumber, secondNumber);
+		double expected = 4.1D;
+
+		assertEquals(expected, actual, () -> firstNumber + " + "+secondNumber+" did not producer "+expected+"!");
+	}
+	
+	@Test
+	void testSqaureRoot() {
+		SimpleMath math = new SimpleMath();
+		double number = 81D;
+		
+		Double actual = math.squareRoot(number);
+		double expected = 9D;
+
+		assertEquals(expected, actual, () -> "Sqaure root of"+ number +" did not producer "+expected+"!");
 	}
 	
 }
