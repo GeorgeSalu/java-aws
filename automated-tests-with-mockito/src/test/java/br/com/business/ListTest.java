ec74ec884bdbe843602607de98e9576cc02ee2e2
GeorgeSalu <george.salu10@gmail.com>
2023-10-08 10:32:47 -0300

    automated-tests-with-mockito - mockando listas

---
+++ automated-tests-with-mockito/src/test/java/br/com/business/ListTest.java
@@ -0,0 +1,58 @@
+package br.com.business;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+public class ListTest {
+
+	@Test
+	void testMockingList_When_SizeIsCalled_ShouldReturn10() {
+		// Given / Arrange
+		List<?> list = mock(List.class);
+		when(list.size()).thenReturn(10);
+		
+		// When / Act && Then / Assert
+		
+		assertEquals(10, list.size());
+	}
+	
+	@Test
+	void testMockingList_When_SizeIsCalled_ShouldReturnMultiplesValues() {
+		// Given / Arrange
+		List<?> list = mock(List.class);
+		when(list.size()).thenReturn(10).thenReturn(20);
+		
+		// When / Act && Then / Assert
+		
+		assertEquals(10, list.size());
+		assertEquals(20, list.size());
+		assertEquals(20, list.size());
+	}
+	
+	@Test
+	void testMockingList_When_SizeIsCalled_ShouldReturnGeorge() {
+		// Given / Arrange
+		var list = mock(List.class);
+		when(list.get(0)).thenReturn("George");
+		
+		// When / Act && Then / Assert
+		
+		assertEquals("George", list.get(0));
+	}
+	
+	@Test
+	void testMockingList_When_GetISCalledWithArgumentMatcher_ShouldReturnGeorge() {
+		// Given / Arrange
+		var list = mock(List.class);
+		when(list.get(anyInt())).thenReturn("George");
+		
+		// When / Act && Then / Assert
+		
+		assertEquals("George", list.get(anyInt()));
+	}
+	
+}
