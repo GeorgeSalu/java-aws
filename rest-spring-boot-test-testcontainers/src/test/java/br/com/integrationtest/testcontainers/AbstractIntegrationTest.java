63013c75107e4f33baa1dc3783ae70208ffb092d
GeorgeSalu <george.salu10@gmail.com>
2023-10-21 15:10:49 -0300

    rest-spring-boot-test-testcontainers - validando a geracao da pagina do swagger ui com testcontainer e rest-assured

--- rest-spring-boot-test-testcontainers/src/test/java/br/com/integrationtest/testcontainers/AbstractIntegrationTest.java
+++ rest-spring-boot-test-testcontainers/src/test/java/br/com/integrationtest/testcontainers/AbstractIntegrationTest.java
@@ -14,30 +14,33 @@ import org.testcontainers.lifecycle.Startables;
 @ContextConfiguration(initializers = AbstractIntegrationTest.Initializer.class)
 public class AbstractIntegrationTest {
 
-	static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+    static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
 
-		static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.0.28");
-		
-		private static void startContainers() {
-			Startables.deepStart(Stream.of(mysql)).join();
-		}
-		
-		private static Map<String, String> createConnectionConfiguration() {
-			return Map.of(
-					"spring.datasource.url", mysql.getJdbcUrl(),
-					"spring.datasource.username", mysql.getUsername(),
-					"spring.datasource.password", mysql.getPassword()
-					);
-		}
-		
-		@Override
-		public void initialize(ConfigurableApplicationContext applicationContext) {
-			startContainers();
-			ConfigurableEnvironment environment = applicationContext.getEnvironment();
-			MapPropertySource testContainers = new MapPropertySource("testcontainers", (Map) createConnectionConfiguration());
-			environment.getPropertySources().addFirst(testContainers);
-		}
-		
-	}
-	
+        static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.0.28");
+        
+        private static void startContainers() {
+            Startables.deepStart(Stream.of(mysql)).join();
+        }
+        
+        private static Map<String, String> createConnectionConfiguration(){
+            return Map.of(
+                "spring.datasource.url", mysql.getJdbcUrl(),
+                "spring.datasource.username", mysql.getUsername(),
+                "spring.datasource.password", mysql.getPassword());
+        }
+        
+        @Override
+        @SuppressWarnings({ "rawtypes", "unchecked" })
+        public void initialize(ConfigurableApplicationContext applicationContext) {
+            startContainers();
+            ConfigurableEnvironment environment = applicationContext.getEnvironment();
+            MapPropertySource testcontainers =
+                new MapPropertySource(
+                    "testcontainers",
+                    (Map) createConnectionConfiguration());
+            
+            environment.getPropertySources().addFirst(testcontainers);
+        }
+        
+    }
 }
---
+++ rest-spring-boot-test-testcontainers/src/test/java/br/com/integrationtests/swagger/SwaggerIntegrationTest.java
@@ -0,0 +1,33 @@
+package br.com.integrationtests.swagger;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import br.com.config.TestConfigs;
+import br.com.integrationtest.testcontainers.AbstractIntegrationTest;
+import br.com.rest.Startup;
+
+
+@SpringBootTest(classes = Startup.class , webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+class SwaggerIntegrationTest extends AbstractIntegrationTest {
+
+    @Test
+    @DisplayName("JUnit test for Should Display Swagger UI Page")
+    void testShouldDisplaySwaggerUiPage() {
+        var content = given()
+            .basePath("/swagger-ui/index.html")
+            .port(TestConfigs.SERVER_PORT)
+            .when()
+                .get()
+            .then()
+                .statusCode(200)
+            .extract()
+                .body()
+                    .asString();
+        assertTrue(content.contains("Swagger UI"));
+    }
+}
